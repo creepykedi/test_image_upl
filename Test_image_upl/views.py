19655732d06fc6655f8a85ee9b93687e881441a7
creepykedi <pumpkinthestrange@gmail.com>
2020-06-21 17:55:47 +0300

    add validators, fix image formats

--- Test_image_upl/views.py
+++ Test_image_upl/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, redirect, HttpResponse, get_object_or_404
+from django.shortcuts import render, HttpResponse, get_object_or_404
+from django.http import HttpResponseBadRequest
 from .forms import UploadImageform
+from django.core.validators import ValidationError
 from .models import Img
 import requests
 from django.core.files.base import ContentFile
@@ -40,33 +42,51 @@ def upload(request):
 
 
 def resize_image(set_width, set_height, set_size, imgbytes):
-    pil_img = Image.open(imgbytes)
+    try:
+        pil_img = Image.open(imgbytes)
+        if pil_img.format not in ('BMP', 'JPG', 'JPEG', 'PNG'):
+            raise ValidationError("Unsupported file type")
+    except ImportError:
+        raise ValidationError
+
     width, height = pil_img.size
     if set_width and set_height:
-        pil_img = pil_img.resize((int(set_width), int(set_height)))
+        try:
+            pil_img = pil_img.resize((int(set_width), int(set_height)))
+        except ValueError:
+            raise Exception("Invalid parameter. Parameter must be a number")
 
     elif set_height:
-        assert set_height != 0
-        resize_by_height = int(set_height)/height
-        pil_img = pil_img.resize((int(width*resize_by_height), int(set_height)))
+        try:
+            resize_by_height = int(set_height)/height
+            pil_img = pil_img.resize((int(width*resize_by_height), int(set_height)))
+        except (ZeroDivisionError, ValueError) as e:
+            raise Exception("Invalid parameter. Parameter must be a positive number which is not 0")
 
     elif set_width:
-        assert set_width != 0
-        resize_by_width = int(set_width)/width
-        pil_img = pil_img.resize((int(set_width), int(height // (1 / resize_by_width))))
+        try:
+            resize_by_width = int(set_width)/width
+            pil_img = pil_img.resize((int(set_width), int(height // (1 / resize_by_width))))
+        except (ZeroDivisionError, ValueError) as e:
+            raise Exception("Invalid parameter. Parameter must be a positive number which is not 0")
 
     in_mem_file = io.BytesIO()
     n = 100
-    pil_img.save(in_mem_file, format='JPEG')
+    Imageformat = pil_img.format
+    pil_img.save(in_mem_file, format=Imageformat)
     imgbytes = in_mem_file.getvalue()
+
     if set_size:
-        while len(imgbytes) > int(set_size):
-            in_mem_file = io.BytesIO()
-            pil_img.save(in_mem_file, format='JPEG', optimize=True, quality=n)
-            n -= 1
-            imgbytes = in_mem_file.getvalue()
-            if n < 2:
-                break
+        try:
+            while len(imgbytes) > int(set_size):
+                in_mem_file = io.BytesIO()
+                pil_img.save(in_mem_file, format=Imageformat, optimize=True, quality=n)
+                n -= 5
+                imgbytes = in_mem_file.getvalue()
+                if n < 5:
+                    break
+        except (ZeroDivisionError, ValueError) as e:
+            raise Exception("Invalid parameter. Parameter must be a positive number which is not 0")
     return imgbytes
 
 
@@ -85,8 +105,11 @@ def image_view(request, image_hash, *args, **kwargs):
             set_size = request.GET["size"]
         imgbytes = resize_image(set_width, set_height, set_size, imgbytes)
 
-    response = HttpResponse(imgbytes, content_type='image/jpeg')
-    response['Cache-Control'] = 'public, max-age=315360'
+    if imgbytes:
+        response = HttpResponse(imgbytes, content_type='image/jpeg')
+        response['Cache-Control'] = 'public, max-age=315360'
+    else:
+        return HttpResponseBadRequest
     return response
 
 
